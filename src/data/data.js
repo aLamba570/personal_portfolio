ee69635d06ae8986fab261a32386ed45544e6a44
aLamba570 <alamba570@gmail.com>
2023-12-27 07:43:41 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/BnR4CCg0/68747470733a2f2f692e6962622e636f2f71706832635a6e2f696d6167652e706e6767.png',
+    title: "Travel App",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/5yKmCjMs/292848953-3146a3a8-53dc-46d4-af74-f29cc0d3cad3.png',
+    title: "Crypto Verse",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/htr87D40/Youtube.png',
+    title: "Youtube ",
+    desc: "",
+    link: "https://youtube-rouge-three.vercel.app/",
+    git: "https://github.com/aLamba570/Youtube",
+  }
+  
+];
